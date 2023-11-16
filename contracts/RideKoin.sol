1986a7e69d606f985dde285e130475f8cdaeaf80
dashuto22 <ashudubey.sep14@gmail.com>
2023-11-15 20:46:00 -0500

    boilerplate

---
+++ contracts/RideKoin.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.16;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract RideKoin is ERC20, Ownable {
+    uint256 public tokenPrice; 
+
+    constructor(uint256 initialPrice)
+        ERC20("RideKoin", "RDK")
+        Ownable(msg.sender)
+    {
+        _mint(msg.sender, 70000000 * 10 ** decimals());
+        tokenPrice = initialPrice;
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    modifier onlyOwnerOrAccountHolder(address user) {
+        require(user == msg.sender || owner() == msg.sender, "Not enough Permissions");
+        _;
+    }
+
+    function balanceOf(address user) public view override onlyOwnerOrAccountHolder(user) returns (uint256) {
+        return super.balanceOf(user);
+    }
+
+    function _balanceOf(address user) external view returns (uint256) {
+        return super.balanceOf(user);
+    } // <-- tHis is for use in ERC1155
+
+    function getEtherBalance(address user) public view onlyOwnerOrAccountHolder(user) returns (uint256) {
+        return user.balance;
+    }
+
+    function setTokenPrice(uint256 newPrice) external onlyOwner {
+        tokenPrice = newPrice;
+    }
+
+    function buyToken(uint256 tokenAmount) public payable {
+        uint256 requiredEther = tokenAmount * tokenPrice;
+        require(msg.value == requiredEther, "Ether sent doesn't match the required amount for tokens");
+        _transfer(owner(), msg.sender, tokenAmount); // Transfer RideKoin tokens from owner to buyer
+    }
+
+
+     function sellToken(uint256 tokenAmount) public {
+        uint256 etherAmount = tokenAmount * tokenPrice;
+        // require(address(owner).balance >= etherAmount, "Contract does not have enough ether to buy the tokens");
+        _transfer(msg.sender, owner(), tokenAmount); 
+        payable(msg.sender).transfer(etherAmount);   
+    }
+
+    function burnFrom(address user, uint256 amount) public {
+        require(user == msg.sender, "You the user can burn his/her tokens");
+        _burn(user, amount);
+    }
+
+
+    function withdraw() external onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    }
+
+    function approveAddress(address spender, uint256 amount) public payable  onlyOwner {
+        _approve(msg.sender, spender, amount);
+    }
+
+}
+
+
---
+++ contracts/Rydeasset.sol
@@ -0,0 +1,158 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.16;
+
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "./RideKoin.sol";
+import "./XclusiveRidePass.sol";
+
+contract RydeAsset is ERC1155, Ownable {
+    uint256 public MINT_PRICE = 10 wei;
+
+    RideKoin public rideKoinContract;
+    XclusiveRydePass public xclusiveRydePassContract;
+
+    uint256 private currentTokenID = 2; 
+
+    mapping(uint256 => string) public tokenDescriptions;
+    enum UserRole { None, Driver, Rider }
+    mapping(address => UserRole) public userRoles;
+
+    constructor(address _rideKoinContractAddress, address _xclusiveRydePassContractAddress) 
+        ERC1155("abcdce") 
+            Ownable(msg.sender)  
+    {
+        rideKoinContract = RideKoin(_rideKoinContractAddress);
+        xclusiveRydePassContract = XclusiveRydePass(_xclusiveRydePassContractAddress);
+    }
+
+    function registerAsDriver() public {
+        userRoles[msg.sender] = UserRole.Driver;
+    }
+
+    function registerAsRider() public {
+        userRoles[msg.sender] = UserRole.Rider;
+    }
+
+    function mintXclusiveRydePass(address to) public payable {
+        xclusiveRydePassContract.safeMint{value: msg.value}(to);
+    }
+
+    function transferXclusiveRydePass(address from, address to, uint256 tokenId) public {
+        xclusiveRydePassContract.transferFrom(from, to, tokenId);
+    }
+
+    function getTokens(address owner) public view returns (uint256[] memory) {
+        return xclusiveRydePassContract.getTokens(owner);
+    }
+
+    function buyRideKoin(uint256 tokenAmount) public payable {
+        rideKoinContract.buyToken{value: msg.value}(tokenAmount);
+        rideKoinContract.transfer(msg.sender, tokenAmount);
+    }
+
+    function sellRideKoin(uint256 tokenAmount) public {
+        rideKoinContract.sellToken(tokenAmount);
+    }
+
+    function withdrawFundsFromRideKoin() public onlyOwner {
+        rideKoinContract.withdraw();
+    }
+
+
+    function getRideKoinBalance(address user) public view returns (uint256) {
+        return rideKoinContract._balanceOf(user);
+    }
+
+    function getXclusiveRydePassCount(address user) public view returns (uint256) {
+        return xclusiveRydePassContract.balanceOf(user);
+    }
+
+
+    function getTokenDescription(uint256 tokenId) external view returns (string memory) {
+        return tokenDescriptions[tokenId];
+    }
+
+        struct SpecialTokenDetail {
+        uint256 rydeKoinAmount;
+        uint256 xclusiveRydePassID;
+    }
+
+    mapping(uint256 => uint256) public specialTokenValues; // The value of each SpecialToken in wei
+
+
+    mapping(uint256 => SpecialTokenDetail) public specialTokenDetails;
+
+    function createSpecialToken(uint256 rydeKoinAmount, uint256 xclusiveRydePassID) public payable {
+        require(rideKoinContract._balanceOf(msg.sender) >= rydeKoinAmount, "Not enough RideKoin");
+        require(xclusiveRydePassContract.ownerOf(xclusiveRydePassID) == msg.sender, "You don't own the specified XclusiveRydePass");
+        rideKoinContract.transferFrom(msg.sender, address(this), rydeKoinAmount);
+        xclusiveRydePassContract.transferFrom(msg.sender, address(this), xclusiveRydePassID);
+
+        specialTokenDetails[currentTokenID] = SpecialTokenDetail({
+            rydeKoinAmount: rydeKoinAmount,
+            xclusiveRydePassID: xclusiveRydePassID
+        });
+
+        _mint(msg.sender, currentTokenID, 1, "");
+        currentTokenID++;
+    } /* Before using this function ensure that the smart contract is approved by both RideKoin and XRP smart contracts as we are authorising the ERC1155
+        Contract to transfer money on behaf of the user. This method will be improved in phase 2.
+    
+    */
+
+    modifier onlyOwnerOrAccount(address account) {
+        require(account == msg.sender || owner() == msg.sender, "Not the owner or the specified ownr");
+        _;
+    }
+
+    function viewOwnedSpecialTokens(address account) public onlyOwnerOrAccount(account) view returns (uint256[] memory) {
+        uint256 tokenCount = balanceOf(account,2);
+        uint256[] memory ownedTokens = new uint256[](tokenCount);
+        
+        uint256 counter = 0;
+        for (uint256 i = 0; i < currentTokenID; i++) {
+            if (balanceOf(account, i) > 0) {
+                ownedTokens[counter] = i;
+                counter++;
+            }
+        }
+        return ownedTokens;
+    }
+
+
+    function returnSpecialToken(uint256 specialTokenID) external {
+       require(balanceOf(msg.sender, specialTokenID) > 0, "You don't own the specified special token");
+        uint256 additionalRideKoinsForXclusivePass = 2;
+        rideKoinContract.transfer(msg.sender, specialTokenDetails[specialTokenID].rydeKoinAmount + additionalRideKoinsForXclusivePass);
+        delete specialTokenDetails[specialTokenID];
+    }
+
+    mapping(uint256 => uint256) public xclusivePassPrices;
+
+    function setXclusivePassPrice(uint256 xclusiveRydePassID, uint256 price) external {
+        require(xclusiveRydePassContract.ownerOf(xclusiveRydePassID) == msg.sender, "You don't own the specified XclusiveRydePass");
+        xclusivePassPrices[xclusiveRydePassID] = price;     
+    }
+
+    function mintBatchXclusiveRydePass(address[] memory to) public payable {
+        require(msg.value == MINT_PRICE * to.length, "Incorrect amount of Ether sent");
+        
+        for (uint256 i = 0; i < to.length; i++) {
+            xclusiveRydePassContract.safeMint{value: MINT_PRICE}(to[i]);
+        }
+    }
+
+    
+
+    function transferBatchXclusiveRydePass(address from, address[] memory to, uint256[] memory tokenId) public {
+        require(to.length == tokenId.length, "Array length mismatch");
+
+        for (uint256 i = 0; i < to.length; i++) {
+            xclusiveRydePassContract.transferFrom(from, to[i], tokenId[i]);
+        }
+
+
+}
+}
